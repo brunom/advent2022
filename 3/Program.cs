bac2cc4f4bc9a6b311f3ce7aba0dc0bfa332929c
Bruno <bruno.uy@gmail.com>
2022-12-04 17:50:31 -0300

    3

---
+++ 3/Program.cs
@@ -0,0 +1,24 @@
+﻿int Prio(char ch)
+{
+    if ('a' <= ch && ch <= 'z')
+        return 1 + ch - 'a';
+    if ('A' <= ch && ch <= 'Z')
+        return 27 + ch - 'A';
+    throw new ArgumentOutOfRangeException();
+}
+
+var sacks = File.ReadLines("input.txt").ToList();
+
+var prios1 =
+    from sack in sacks
+    let comp0 = sack.Take(sack.Count() / 2)
+    let comp1 = sack.Skip(sack.Count() / 2)
+    select Prio(comp0.Intersect(comp1).Single());
+Console.WriteLine(prios1.Sum());
+
+var prios2 =
+    from ig in Enumerable.Range(0, sacks.Count() / 3)
+    let g = sacks.Skip(3*ig).Take(3)
+    let common = g.Aggregate<IEnumerable<char>>(Enumerable.Intersect)
+    select Prio(common.Single());
+Console.WriteLine(prios2.Sum());
