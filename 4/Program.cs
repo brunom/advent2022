d239572de3b9844b0f973c5ead489e433a9c04da
Bruno <bruno.uy@gmail.com>
2022-12-04 17:50:44 -0300

    4

---
+++ 4/Program.cs
@@ -0,0 +1,9 @@
+﻿var lines = File.ReadLines("input.txt").ToList();
+var p =
+    from line in lines
+    let elves = line.Split(',').Select(x => x.Split('-').Select(y => int.Parse(y)).ToArray()).ToArray()
+    // where elves[0][0] <= elves[1][0] && elves[1][1] <= elves[0][1]
+    // ||  elves[1][0] <= elves[0][0] && elves[0][1] <= elves[1][1]
+    where elves[0][1] >= elves[1][0] && elves[1][1] >= elves[0][0]
+    select elves;
+Console.WriteLine(p.Count());
